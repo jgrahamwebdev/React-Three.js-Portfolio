2d1e0d5c8ad26d873448078b391d7545cf7187d1
J. Graham <jgraham@Js-MacBook-Air.local>
2023-03-08 02:47:02 -0700

    changed scroll down icon to arrow

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -30,10 +30,10 @@ const Hero = () => {
 
             <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
               <a href='#about'>
-              <div className='w-[35px] h-[64px] rounded-3xl border-4 border-white flex justify-center items-start p-2'>
+              {/* <div className='w-[35px] h-[64px] rounded-3xl border-4 border-white flex justify-center items-center m-auto p-2'>
                 <motion.div
               animate={{
-                y: [0, 24, 0],
+                y: [0, 22, 0],
               }}
               transition={{
                 duration: 1.5,
@@ -42,7 +42,16 @@ const Hero = () => {
               }}
               className='w-3 h-3 rounded-full bg-white mb-1'
             />
-            </div>
+            </div> */}
+           <motion.div className="text-center text-[3rem]" animate={{
+                y: [0, 22, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}>&darr;</motion.div>
+              
             </a>
           </div>
 
