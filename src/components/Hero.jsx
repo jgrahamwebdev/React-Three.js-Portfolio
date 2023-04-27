5a51623bf135d76966f23a2206677bc7dea0f4a4
J. Graham <jgwebdev38@gmail.com>
2023-04-27 02:30:17 -0600

    Fixed images

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
         <section className="relative w-full h-screen mx-auto">
 
             {/* TITLE SECTION */}
-            <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auot flex flex-row items-start gap-5`}>
+            <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
                 <div className="flex flex-col justify-center items-center mt-5">
                     <div className="w-5 h-5 rounded-full bg-[#3742fa]"/>
                     <div className="w-1 sm:h-80 h-40 violet-gradient"/>
